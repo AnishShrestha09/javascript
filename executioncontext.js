e93b9762b84f293a33fbbd94631e53bcf7f55554
HARIVANSH <shranish999@gmail.com>
2026-08-30 07:51:04 +0545

    how code actually learns debug

---
+++ executioncontext.js
@@ -0,0 +1,18 @@
+var a = 5;
+let b = 6;
+console.log(a + b);
+
+function fun1() {
+ let a = 5
+ let b= 6
+ return a+b
+}
+
+let result = fun1();
+console.log(result);
+
+
+// fun2()
+// var fun2 = () =>{
+//     console.log("hwllo dosto");
+// }
