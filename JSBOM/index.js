2569e4500f372482145f4b7b2841468f7f1a76a7
HARIVANSH <shranish999@gmail.com>
2026-09-20 19:14:20 +0545

    local storage

---
+++ JSBOM/index.js
@@ -0,0 +1,29 @@
+// let storage = localStorage.setItem("num",1)
+
+
+
+// let result = localStorage.getItem("num")
+// console.log(result)
+
+// let result2 = localStorage.key("0")
+// console.log(result2) 
+
+// localStorage.removeItem("num")
+
+
+// clear the local storage completely
+// localStorage.setItem("num",1)
+// localStorage.setItem("num2",1)
+// localStorage.setItem("num3",1)
+
+// const btn = document.getElementById("clear")
+// btn.addEventListener("click",()=>{
+//     localStorage.clear()
+// })
+
+
+// session storage
+
+
+
+
